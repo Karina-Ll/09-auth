0d3c3e0f050f1de65fcb54f0625aaf7b7b1c3e86
Karina <karina24032006.09@gmail.com>
2026-05-14 13:40:38 +0300

    fix: update api imports in private routes

--- app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
+++ app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { fetchNotes } from "../../../../lib/api";
+import { fetchNotes } from "../../../../lib/api/clientApi";
 import NoteList from "../../../../components/NoteList/NoteList";
 import Pagination from "../../../../components/Pagination/Pagination";
 import SearchBox from "../../../../components/SearchBox/SearchBox";
